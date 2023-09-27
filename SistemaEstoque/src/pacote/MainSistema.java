5e05c483ff6a5309834d9dd93214982097769617
Donnovan Machado <donnovanriddle19@gmail.com>
2023-09-27 17:30:11 -0300

    Edição e Cadastro

--- SistemaEstoque/src/pacote/MainSistema.java
+++ SistemaEstoque/src/pacote/MainSistema.java
@@ -6,16 +6,13 @@ import java.util.Scanner;
 public class MainSistema {
 
 	public static void main(String[] args) {
-		
+
 		Integer opcaoSelecionada = Integer.MAX_VALUE;
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Produto> produtos = new ArrayList<>();
-		
+
 		while (opcaoSelecionada != 0) {
-			/**
-			 * Voce nao precisa seguir esta ordem, desde que seu sistema respeite a logica
-			 * de incluir, alterar, listar e excluir de alguma forma.
-			 */
+
 			System.out.println("------ Qual Operação deseja realizar? ------");
 			System.out.println("0 SAIR");
 			System.out.println("1 EDITAR");
@@ -23,58 +20,57 @@ public class MainSistema {
 			System.out.println("3 EXCLUIR");
 			System.out.println("4 LISTAR");
 
-			opcaoSelecionada = Integer.valueOf(sc.nextLine());			
-			
+			opcaoSelecionada = Integer.valueOf(sc.nextLine());
+
 			switch (opcaoSelecionada) {
 			case 0: {
 				break;
 			}
 			case 1: {
-				
-				System.out.println("Nome: ");
-				String nome = sc.nextLine();
-				System.out.println("Marca: ");
-				String marca = sc.nextLine();
 
-				System.out.println("Preço: ");
-				String preco = sc.nextLine();
-				System.out.println("Estoque: ");
-				String qtdEstoque = sc.nextLine();
-				
-				
-				Produto produto = new Produto();
-				produto.setNome(nome);
-				produto.setPreco(Float.valueOf(preco));
-				/*TERMINAR
-				 * 
-				 
-				
-
-				produto.add(produtos);
-
+				System.out.println("Digite o id do Produto que deseja Editar:");
+				Long id = Long.valueOf(sc.nextLine());
+				for (Produto produto : produtos) {
+					if (id == produto.getId()) {
+						// editar();
+					} else {
+						System.out.println("Id inválido");
+					}
+				}
 			}
 			case 2: {
-				System.out.println("Digite o cpf da pessoa que deseja alterar:");
-				// finalizar codigo
+				// cadastrar();
+
 			}
 			case 3: {
-				System.out.println("Digite o cpf da pessoa que deseja excluir:");
-				// finalizar codigo
+				System.out.println("Digite o id do Produto que deseja excluir:");
+
 			}
 			case 4: {
 				// Saida de dados (output)
 				System.out.println("Listagem de pessoas cadastradas: ");
-				for (Pessoa pessoa : pessoas) {
-					System.out.println("Cpf: " + pessoa.getCpf());
-					System.out.println("Nome: " + pessoa.getNome());
-				}
-			}
+
+			 }
 
 			}
-				*/
+
 		}
 		sc.close();
+
+	}
+	
+	public static void editar() {
 		
 	}
+	public static void cadastrar() {
+		
+		for (Produto produto : produtos) {
+			System.out.println("Nome: " + produto.getNome());
+			System.out.println("Marca: " + produto.getNome());
+			System.out.println("Preço: " + produto.getNome());
+			System.out.println("Quantidade de Estoque: " + produto.getNome());
+		}
+	}
+	
 
 }
