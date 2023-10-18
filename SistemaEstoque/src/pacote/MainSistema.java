109739188e81d7d9985595eeb49c171f669ae550
marianaacb <mariana.cb@aluno.ifsc.edu.br>
2023-10-18 14:22:23 -0300

    Erros corrigidos
    Correções feitas por mim e pelo Donnovan juntos

--- SistemaEstoque/src/pacote/MainSistema.java
+++ SistemaEstoque/src/pacote/MainSistema.java
@@ -1,4 +1,5 @@
 package pacote;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,56 +14,58 @@ public class MainSistema {
 
 			System.out.println("------ Qual Operação deseja realizar? ------");
 			System.out.println("0 SAIR");
-			System.out.println("1 EDITAR");
-			System.out.println("2 CADASTRAR");
+			System.out.println("1 CADASTRAR");
+			System.out.println("2 EDITAR");
 			System.out.println("3 EXCLUIR");
 			System.out.println("4 LISTAR");
 
 			opcaoSelecionada = Integer.valueOf(sc.nextLine());
 
 			switch (opcaoSelecionada) {
-			case 0: {
-				sc.close();
-				break;
-			}
-			case 1: {
-				System.out.println("Digite o id do Produto que deseja Editar:");
-				Long id = Long.valueOf(sc.nextLine());
-				for (Produto produto : produtos) {
-					if (id == produto.getId()) {
-						editar(produtos);
-					} else {
-						System.out.println("Id inválido");
-					}
-				}
-			}
-			case 2: {
-				cadastrar(produtos);
-
-			}
-			case 3: {
-				System.out.println("Digite o ID do produto a ser editado:");
-				long idProduto = Long.valueOf(sc.nextLong());
-				System.out.println("VOCÊ TEM CERTEZA QUE QUER APAGAR " + idProduto + "?");
-				System.out.println("[S] Sim");
-				System.out.println("[N] Não");
-				String certeza = String.valueOf(sc.nextLine());
-				while (certeza != "S" && certeza != "N") {
-					System.out.println("Resposta inválida. Tente novamente");
-					certeza = String.valueOf(sc.nextLine());
-				}
-				if (certeza == "S")
-
-					excluir(produtos, idProduto);
-				else
+				case 0: {
+					System.out.println("Penguim anao mandou tchau");
+					sc.close();
 					break;
-				else
-					System.out.println("Resposta inválida. Tente novamente");
-			}
-			case 4: {
-				listagem(produtos);
-				break;
-			}
+				}
+				case 1: {
+					System.out.println("sada");
+					cadastrar(produtos);
+					break;
+				}
+				case 2: {	
+					System.out.println("Digite o id do Produto que deseja Editar:");
+					Long id = Long.valueOf(sc.nextLine());
+					for (Produto produto : produtos) {
+						if (id == produto.getId()) {
+							editar(produtos);
+						} else {
+							System.out.println("Id inválido");
+						}
+					}
+					sc.close();
+	
+				}
+				case 3: {
+					System.out.println("Digite o ID do produto a ser editado:");
+					long idProduto = Long.valueOf(sc.nextLong());
+					System.out.println("VOCÊ TEM CERTEZA QUE QUER APAGAR " + idProduto + "?");
+					System.out.println("[S] Sim");
+					System.out.println("[N] Não");
+					String certeza = String.valueOf(sc.nextLine());
+					while (certeza != "S" || certeza != "N") {
+						System.out.println("Resposta inválida. Tente novamente");
+						certeza = String.valueOf(sc.nextLine());
+					}
+					if (certeza == "S")
+	
+						excluir(produtos, idProduto);
+					else
+						break;
+				}
+				case 4: {
+					listagem(produtos);
+					break;
+				}
 
 			} // fim do switch
 		} // fim while
@@ -90,8 +93,7 @@ public class MainSistema {
 			produto.setQtdEstoque(qtdEstoque);
 		}
 		sc.close();
-	}
-	}// fim do main
+	}// fim do cadastrar
 
 	// EDITAR
 	public static void editar(ArrayList<Produto> produtos) {
@@ -101,39 +103,28 @@ public class MainSistema {
 		System.out.println("Qual campo você deseja editar?");
 		campo = sc.nextLine();
 		Integer campoNumber = Integer.valueOf(campo);
-		Float campoNumberF = Float.valueOf(campo);
-
-	// EXCLUIR
-	public static void excluir(ArrayList<Produto> produtos, long idProduto) {
-		for (Produto produto : produtos) {
-			
+		Float campoNumberF = Float.valueOf(campo);		
+		for (Produto produto : produtos) {	
 			if (campo.toLowerCase() == produto.getNome()) {
 				String nome;
 				System.out.println("Qual será o novo nome? ");
 				nome = sc.nextLine();
 				produto.setNome(nome);
-			} 
-			else if (campo.toLowerCase() == produto.getMarca()) {
+			} else if (campo.toLowerCase() == produto.getMarca()) {
 				String marca;
 				marca = sc.nextLine();
 				produto.setMarca(marca);
-			}
-			else if (campoNumberF == produto.getPreco()) {
+			} else if (campoNumberF == produto.getPreco()) {
 				Float preco;
 				preco = sc.nextFloat();
 				produto.setPreco(preco);
-				
-			}
-			else if (campoNumber == produto.getQtdEstoque()) {
+			} else if (campoNumber == produto.getQtdEstoque()) {
 				Integer qtdEstoque;
 				qtdEstoque = sc.nextInt();
 				produto.setQtdEstoque(qtdEstoque);
-			}
-			
+			}			
 		}
-		
-		sc.close();
-	}
+	} // fim do editar
 
 	// LISTAGEM
 	public static void listagem(ArrayList<Produto> produtos) {
@@ -146,7 +137,7 @@ public class MainSistema {
 			System.out.println("Quantidade em estoque: " + produto.getQtdEstoque());
 			System.out.println("------");
 		}
-	}
+	} // fim da listagem
 
 	// EXCLUIR
 	public static void excluir(ArrayList<Produto> produtos, long idProduto) {
@@ -161,6 +152,6 @@ public class MainSistema {
 				break;
 			}
 		}
-	}
+	} // fim do excluir
 
-}
+} // fim do programa
